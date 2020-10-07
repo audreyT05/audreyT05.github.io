fd1ab62819d9ebd7fa8b5bc82206ad62b862ddef
audreyT05 <audreyT05@users.noreply.github.com>
2020-10-07 04:02:30 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -1,14 +1,28 @@
 let myFullName = "Bill Farmer"; // 10 characters in name...not including space
 // 1 Change the content of the div with class "header" to "[Your name]'s Lab 4"
+const myHeading = document.querySelector('.header');
+myHeading.textContent = "Audrey's Lab 4";
 
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
-
+const div = document.querySelector('.content');
+const st = document.createElement('p');
+const nd = document.createElement('p');
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
+const name = 'Audrey Tsang';
+const len = (name.length-1);
+const fpara = document.createTextNode('My name has ' +len+ ' characters.')
+st.appendChild(fpara);
+div.appendChild(st);
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
+const letter = name.charAT(2);
+const spara = document.createTextNode('1=This is letter');
+nd.appendChild(spara);
+div.appendChild(nd);
+
 
 // 6 Add a new line to your second paragraph
 
