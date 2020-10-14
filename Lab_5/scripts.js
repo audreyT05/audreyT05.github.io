6b14255bf56057624023cff3cf93c6692d40582b
audreyT05 <audreyT05@users.noreply.github.com>
2020-10-14 06:56:04 -0400

    Update scripts.js

--- Lab_5/scripts.js
+++ Lab_5/scripts.js
@@ -247,6 +247,22 @@ const CountriesName = ["Afghanistan",
 "Zambia",
 "Zimbabwe",
 "Åland Islands"];
+
+let CountryCode = 
+["AF", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH",
+"BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "CV",
+"KH", "CM", "CA", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CD", "CG", "CK", "CR", "HR", "CU", "CW", "CY",
+"CZ", "CI", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "SZ", "ET", "FK", "FO", "FJ", "FI", "FR", "GF",
+"PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM",
+"VA", "HN", "HK", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI",
+"KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MG", "MW", "MY", "MV", "ML", "MT",
+"MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC",
+"NZ", "NI", "NE", "NG", "NU", "NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT",
+"PR", "QA", "MK", "RO", "RU", "RW", "RE", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS",
+"SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SE", "CH", "SY", "TW",
+"TJ", "TZ", "TH", "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "UM", "US", "UY",
+"UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW", "AX"];
+
 //(1)Add a header to the lab with "YourName's List Of Countries"
 //(2)Using JS, inject an ordered list into the div with the class "content"
 //(3)Give your new ordered list the class "countries"
@@ -273,17 +289,21 @@ function random(){
             CountriesIndex.push(random);
         }
 
-
+//(8)Display the name of each country in a normal font weight
+//(9)Display the country code for each in a bold font weight
         CreateP = document.createElement("p1");
-        CovertedName = document.createTextNode(CountriesName[random]);
+        a = CountriesName[random];
+        b = CountryCode[random];
+        CovertedName = document.createTextNode(a +","+ b.bold());
         CreateP.appendChild(CovertedName);
     
         list = document.createElement("li");
         list.appendChild(CreateP);
         ContentList.appendChild(list);
     }
+    //(10)Log the unselected countries to your console
+    for (i =0; i<CountriesName.length; i++){
+        if(CountriesName[i]!= ContentList[i]);
+        {console.log(CountriesName[i]);}
+    }
 }
-
-//(8)Display the name of each country in a normal font weight
-//(9)Display the country code for each in a bold font weight
-
